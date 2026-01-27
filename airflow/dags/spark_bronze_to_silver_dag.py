ad5468cb21c4a17761d90f009f97011985c10d76
MAHMOUDMAMDOH8 <your-email@example.com>
2026-01-27 13:56:36 +0200

    fix docker volumn

--- airflow/dags/spark_bronze_to_silver_dag.py
+++ airflow/dags/spark_bronze_to_silver_dag.py
@@ -8,25 +8,16 @@ from airflow.utils.dates import days_ago
 # Note: Using docker exec to run spark-submit in spark-master container
 # The spark-shared volume is mounted at /opt/spark/shared in Spark containers
 spark_submit_command = (
-    "docker exec spark-master /opt/spark/bin/spark-submit "
-    "--master spark://spark-master:7077 "
-    "--packages org.apache.iceberg:iceberg-spark-runtime-3.4_2.12:1.5.0,"
-    "org.apache.hadoop:hadoop-aws:3.3.4,"
-    "com.amazonaws:aws-java-sdk-bundle:1.12.262 "
-    "--conf spark.hadoop.fs.s3a.endpoint=https://expert-pancake-jv9wx6vww5w25gj4-4566.app.github.dev/ "
-    "--conf spark.hadoop.fs.s3a.access.key=test "
-    "--conf spark.hadoop.fs.s3a.secret.key=test "
-    "--conf spark.hadoop.fs.s3a.path.style.access=true "
-    "--conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem "
-    "--conf spark.hadoop.fs.s3a.connection.ssl.enabled=true "
-    "--conf spark.sql.catalog.local=org.apache.iceberg.spark.SparkCatalog "
-    "--conf spark.sql.catalog.local.type=hadoop "
-    "--conf spark.sql.catalog.local.warehouse=s3a://telecomlakehouse/iceberg "
-    "--driver-memory 1g "
-    "--executor-memory 2g "
-    "--executor-cores 2 "
-    "--num-executors 2 "
-    "/opt/spark/shared/from_bronze_to_silver.py"
+        "spark-submit "
+        "--master spark://spark-master:7077 "
+        "--packages org.apache.iceberg:iceberg-spark-runtime-3.4_2.12:1.5.0,org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 "
+        "--conf spark.hadoop.fs.s3a.endpoint=https://expert-pancake-jv9wx6vww5w25gj4-4566.app.github.dev/ "
+        "--conf spark.hadoop.fs.s3a.access.key=test "
+        "--conf spark.hadoop.fs.s3a.secret.key=test "
+        "--conf spark.hadoop.fs.s3a.path.style.access=true "
+        "--conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem "
+        "--conf spark.hadoop.fs.s3a.connection.ssl.enabled=true "
+        "/opt/spark/shared/from_bronze_to_silver.py"
 )
 
 # Default arguments for the DAG
